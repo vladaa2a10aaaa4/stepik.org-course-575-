4dd2d404b6116589ad09bdb5a6434f7f308eff96
Vlada <135242631+vladaa2a10aaaa4@users.noreply.github.com>
2024-10-11 17:50:53 +0300

    1.4.5.py

---
+++ 1.4.5.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+chrome_options = Options()
+chrome_options.add_argument('--ignore-certificate-errors')
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://suninjuly.github.io/blog_example.html')
+time.sleep(10)
